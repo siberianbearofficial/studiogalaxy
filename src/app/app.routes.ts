029f78635cd98fdb68c479b6c46e5bd360e6fff8
Aleksei Orlov <alexeyorlov65@gmail.com>
2024-10-13 18:45:15 +0300

    setup redirect to landing page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './presentation/pages/landing-page/landing-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', title: 'Студия звукозаписи Галактика', component: LandingPageComponent },
+    { path: '**', redirectTo: '/', pathMatch: 'full' },
+];
